ae0248e2dcac081b2296af34ca85a77ac5f53f4e
rayalva407 <rayalva407@gmail.com>
2021-05-09 17:13:22 -0600

    formats displayed time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
-import Stopwatch from './components/Stopwatch'
 
 function App() {
 
-  const [seconds, setSeconds] = useState(0)
+  const [time, setTime] = useState(0)
   const [start, setStart] = useState(false)
 
   useEffect(() => {
-    const stopwatch = setInterval(() => {
-      setSeconds(seconds + 1);
-    }, 1000);
+    let interval = null;
 
-    return() => clearInterval(stopwatch)
-  })
+    if(start) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10)
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval)
+  }, [start])
 
   return (
     <div className="App">
       <h1>Stopwatch</h1>
-      <Stopwatch seconds={seconds} start={start} setSeconds={setSeconds} setStart={setStart} />
+      <h1>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + (time / 10) % 1000).slice(-2)}</span>
+      </h1>
+      <div>
+        <button onClick={() => setStart(true)}>Start</button>
+        <button onClick={() => setStart(false)}>Stop</button>
+        <button onClick={() => {setTime(0); setStart(false);}}>Reset</button>
+      </div>
     </div>
   );
 }
